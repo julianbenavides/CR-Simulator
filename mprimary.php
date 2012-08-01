5625d8280a90b1ee0ce588835a578395c9ec81ce
Julian Benavides <julianbenavides@gmail.com>
2012-08-01 15:50:55 -0500

    Layout added

--- mprimary.php
+++ mprimary.php
@@ -1,4 +1,15 @@
 <?php
-include "config.php";
-
+	include "config.php";
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Mobile Primary Users - CR Simulator</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<script src="js/scripts.js">
+</head>
+<body>
+	<p></p>
+</body>
+</html>
--- msecondary.php
+++ msecondary.php
@@ -1,4 +1,35 @@
 <?php
-include "config.php";
-
+	include "config.php";
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Mobile Secondary User - CR Simulator</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<script src="js/scripts.js">
+</head>
+
+<body>
+	<div class="wrapper">
+		<div class="header"></div>
+		<div class="leftcolumn"></div>
+		<div class="content">
+			
+			<?php 
+				/* following statement will define which layout to show possible layouts:
+				Startup layout (to choose between single or multiple users
+				Single Primary User Simulator Layout
+				Multiple Primary Users Simulator Layout
+				Simulator in process - constantly refreshing */
+				
+				
+				
+			?>
+			
+			
+		</div>
+		<div class="footer"></div>
+	</div>
+</body>
+</html>
